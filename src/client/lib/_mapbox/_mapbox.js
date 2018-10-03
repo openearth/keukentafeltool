641f8356852de068891c42c0b9bea812a11a1183
Marko Malis <markomalis@hotmail.com>
2018-10-03 20:44:16 +0200

    refactor(_mapbox): use process.env.MAPBOX_TOKEN when setting mapboxgl.accessToken

--- src/client/lib/_mapbox/_mapbox.js
+++ src/client/lib/_mapbox/_mapbox.js
@@ -5,7 +5,7 @@ let mapboxgl
 if (process.browser) {
   // use old skool `require` because there is no support for dynamic `import`
   mapboxgl = require('mapbox-gl')
-  mapboxgl.accessToken = 'pk.eyJ1Ijoic2lnZ3lmIiwiYSI6ImNqbHcwcHFjNjBsdGIza3F1dW5iZjhhY2EifQ.zZkkozZlSRtmB9VgpR9HiQ'
+  mapboxgl.accessToken = process.env.MAPBOX_TOKEN
 }
 
 export default mapboxgl
