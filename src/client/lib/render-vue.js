213d10622f0b0091006d08acd080664a97837152
Marko Malis <markomalis@hotmail.com>
2018-10-04 13:49:41 +0200

    docs(render-vue): add documentation explaining the functionality and usefulness of this module

--- src/client/lib/render-vue.js
+++ src/client/lib/render-vue.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 
+/**
+ * Renders a Vue component to an HTML element.
+ * This function lets us write components in Vue, which we then can render to HTMLElements when we need this.
+ * @param {Vue.Component} baseComponent - The title of the book.
+ * @param {Object} propsData - The author of the book.
+ * @returns {HTMLElement}
+ */
 export default function (baseComponent, propsData) {
   let Component = Vue.extend(baseComponent)
   let instance = new Component({ propsData })
