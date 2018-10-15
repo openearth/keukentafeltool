aa8a167076026dec071d33f6477f2c0c80c7e7e3
Jasper Moelker <j.b.moelker@activeids.nl>
2018-10-15 15:29:14 +0200

    Add farm routes with nav and panel

---
+++ src/client/pages/farm.vue
@@ -0,0 +1,33 @@
+<template>
+  <nuxt-child v-if="isIndex" />
+  <div
+    v-else
+    class="farm-page__panel"
+  >
+    <farm-nav />
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+import { FarmNav } from '../components'
+
+export default {
+  components: { FarmNav },
+  computed: {
+    isIndex() { return this.$route.name === 'farm' }
+  },
+}
+</script>
+
+<style>
+@import "../components/app-core/variables.css";
+
+.farm-page__panel {
+  min-width: 50%;
+  height: 100%;
+  padding: 16px;
+  overflow-y: auto;
+  background-color: var(--background-light);
+}
+</style>
---
+++ src/client/pages/farm/output.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>output</h2>
+</template>
