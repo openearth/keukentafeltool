bddd05d4e19ebe009514bd2c062455150543db2b
Jasper Moelker <j.b.moelker@activeids.nl>
2018-10-16 09:08:39 +0200

    Redirect all non-matching routes to home page
    to prevent 404's

---
+++ src/client/pages/*.vue
@@ -0,0 +1,15 @@
+<script>
+/**
+ * Redirect all non-matching routes to home page, to prevent 404's.
+ * Borrowed from https://github.com/nuxt/nuxt.js/issues/1691#issuecomment-330790116
+ */
+export default {
+  asyncData ({ redirect }) {
+    return redirect('/')
+  },
+  render(createElement) {
+    // this route doesn't need a template, but causes errors otherwise
+    return createElement('div')
+  },
+}
+</script>
