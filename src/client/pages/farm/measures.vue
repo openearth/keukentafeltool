64962136cbd996499f377b220a0cf9dd29eb3b39
Stefan Kool <stefankool@MacBook-Pro-van-Stefan.local>
2018-10-17 10:51:38 +0200

    refactor pr comments

--- src/client/pages/farm/measures.vue
+++ src/client/pages/farm/measures.vue
@@ -1,33 +1,33 @@
 <template>
-  <div
-    v-if="measures"
-    class="measures-list">
+  <div>
     <div
-      v-for="(measuregroup, index) in measures"
-      :key="index"
-      class="measure-group">
-      <h2 class="measure-group__title">{{ measuregroup.group }}</h2>
+      v-if="measures"
+      class="measures-list">
       <div
-        v-for="(measure, index) in measuregroup.measures"
-        :key="index">
+        v-for="(measuregroup, index) in measures"
+        :key="index"
+        class="measure-group">
+        <h2 class="measure-group__title">{{ measuregroup.group }}</h2>
         <div
-          class="measure-select">
-          <div class="measure-select__button">
-            <div class="measure-select__header">
-              <strong>{{ measure.title }}</strong>
-              <div class="measure-select__caption">KIES</div>
+          v-for="(measure, index) in measuregroup.measures"
+          :key="index">
+          <div
+            class="measure-select">
+            <div class="measure-select__button">
+              <div class="measure-select__header">
+                <strong>{{ measure.title }}</strong>
+                <div class="measure-select__caption">KIES</div>
+              </div>
             </div>
           </div>
-          <!--<div class="measure-select__parcels">
-            <span class="measure-select__label">PERCELEN:</span>
-            <div class="parcel-chip">36452735 <md-icon class="parcel-chip__icon">clear</md-icon></div>
-          </div>-->
         </div>
+        <footer-bar>
+          <template>Kies een maatregel</template>
+        </footer-bar>
       </div>
-      <footer-bar>
-        <template>Kies een maatregel</template>
-        <!--<template>Klik op een perceel om deze maatregel toe te voegen</template>-->
-      </footer-bar>
+    </div>
+    <div v-else>
+      Error no measures found.
     </div>
   </div>
 </template>
@@ -87,39 +87,5 @@ export default {
     vertical-align: baseline;
     font-size: 12px;
     color: transparent;
-  }
-  .measure-select__parcels {
-    padding: 12px 18px;
-    font-size: 12px;
-    color: #666666;
-    background: #ececec;
-  }
-  .measure-select__label {
-    margin-right: 5px;
-  }
-  .parcel-chip {
-    border-radius: 2px;
-    padding: 0 5px;
-    display: inline-block;
-    color: #000000;
-    background: #C4C4C4;
-    border: 1px solid #B0B0B0;
-    cursor: pointer;
-  }
-  .parcel-chip:hover {
-    color: #ffffff;
-    background: #0038FF;
-    border-color: #0038FF;
-    transition: all ease-in-out 240ms;
-  }
-  .parcel-chip:hover .md-icon {
-    color: #ffffff;
-  }
-  .parcel-chip__icon {
-    width: 14px;
-    min-width: 14px;
-    height: 14px;
-    font-size: 14px!important;
-    vertical-align: text-bottom;
   }
 </style>
