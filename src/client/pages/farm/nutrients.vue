38da585b442f9f17027a8ebbffe3bfb3fd5e91b1
Marko Malis <markomalis@hotmail.com>
2018-11-06 11:37:18 +0100

    WIP: add initial layers on nutrients view

--- src/client/pages/farm/nutrients.vue
+++ src/client/pages/farm/nutrients.vue
@@ -17,8 +17,12 @@
 
 <script>
 import { mapGetters, mapState } from 'vuex'
+
 import initMapState from '../../lib/mixins/init-map-state'
+import layerFactory from '../../lib/_mapbox/layer-factory'
+import parcelColors from '../../lib/_mapbox/parcel-colors'
 import requireFeatures from '../../lib/mixins/require-features'
+
 import { NutrientsTable, NutrientsTotals } from '../../components'
 
 export default {
@@ -53,6 +57,24 @@ export default {
   },
   methods: {
     initMapState() {
+      const overlay = layerFactory.parcels()
+      this.$store.dispatch('mapbox/overlays/add', overlay)
+      this.$store.dispatch('mapbox/overlays/setOpacity', { id: overlay.id, opacity: 0.3 })
+
+      this.features.forEach((feature, index) => {
+        this.$store.dispatch('mapbox/features/add', feature)
+        this.$store.dispatch('mapbox/features/setStyle', {
+          id: feature.id,
+          styleOption: 'fill-color',
+          value: parcelColors(feature.properties.gewascategorie)
+        })
+        this.$store.dispatch('mapbox/features/setStyle', {
+          id: feature.id,
+          styleOption: 'fill-opacity',
+          value: 1
+        })
+      })
+
       this.$emit('fitFeatures')
     },
   }
